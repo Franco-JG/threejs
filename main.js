2a284e3e31715104ff1b868758b4f4567e91a5ef
Franco-JG <jeftefranco@outlook.com>
2024-09-05 15:34:09 -0600

    obj model added

--- main.js
+++ main.js
@@ -2,10 +2,11 @@ import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { RGBELoader } from "three/examples/jsm/loaders/RGBELoader";
+import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader";
 
 const scene = new THREE.Scene()
 const axes = new THREE.AxesHelper(5)
-//? scene.add(axes)
+scene.add(axes)
 
 //* Creación de la cámara
 const fov = 75
@@ -23,13 +24,12 @@ renderer.shadowMap.enabled = true;  //! Activamos las sombras en el renderer
 document.body.appendChild(renderer.domElement)
 
 //* HDRI
-// const loader = new RGBELoader();
-// console.log(loader)
-// loader.load('dancing_hall_2k.hdr', function(texture) {
-//     texture.mapping = THREE.EquirectangularReflectionMapping;
-//     scene.background = texture;
-//     scene.environment = texture;
-// });
+const loaderHDRI = new RGBELoader();
+loaderHDRI.load('cannon_1k.hdr', function(texture) {
+    texture.mapping = THREE.EquirectangularReflectionMapping;
+    scene.background = texture;
+    scene.environment = texture;
+});
 
 //* Controles para arrastrar la vista de cámara
 const controls = new OrbitControls(camera, renderer.domElement)
@@ -37,14 +37,14 @@ controls.enableDamping = true;
 controls.enableZoom = true;
 
 //* Creación del punto de luz (foco)
-const light = new THREE.PointLight(0xffffff, 100, 100);
-light.position.set(-2,7,-2)
+const light = new THREE.PointLight(0xffffff, 300, 100);
+light.position.set(-2,10,-2)
 light.castShadow = true;  //! Activamos la proyección de sombras para la luz
 scene.add(light)
 
 const sphereSize = 1;
 const pointLightHelper = new THREE.PointLightHelper( light, sphereSize );
-// ? scene.add( pointLightHelper );
+scene.add( pointLightHelper );
 
 //! Ajustes de sombras para la luz
 light.shadow.mapSize.width = 512;  
@@ -61,12 +61,41 @@ plane.receiveShadow = true;  //! El plano debe recibir sombras
 scene.add(plane)
 
 //* Creacion del cubo
-const cubeGeometry =  new THREE.BoxGeometry(2,2,2)
-const cubeMaterial = new THREE.MeshStandardMaterial({color: new THREE.Color("rgb(211, 145, 227)")}) //! Cambiamos a MeshStandardMaterial
-const cube = new THREE.Mesh(cubeGeometry, cubeMaterial)
-cube.position.set(0,2,0) 
-cube.castShadow = true;  //! El cubo proyectará sombras
-scene.add(cube)
+const sphereGeometry =  new THREE.SphereGeometry(2, 32, 32)
+const sphereMaterial = new THREE.MeshStandardMaterial({
+  metalness: 1,
+  roughness: 0,
+  envMapIntensity: 10,  // Controlar la intensidad del reflejo del mapa de entorno
+
+}) //! Cambiamos a MeshStandardMaterial
+const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial)
+sphere.position.set(0,2,-3) 
+sphere.castShadow = true;  //! El cubo proyectará sombras
+console.log(sphereMaterial);
+scene.add(sphere)
+
+//* Carga del .obj
+const loader = new OBJLoader();
+loader.load(
+  'x-garraP - v4.obj',
+  (object) => {
+    object.traverse((child) => {
+      if (child.isMesh) {
+        child.position.set(0,2,6)
+        child.castShadow = true;    // Habilitar que el objeto proyecte sombra
+        // child.receiveShadow = true; // Opcional: puede recibir sombras también
+      }
+    });
+    scene.add(object);  // Añadir el objeto cargado a la escena
+  }
+  ,
+  (xhr) => {
+    console.log((xhr.loaded / xhr.total * 100) + '% cargado .obj'); // Progreso de carga
+  },
+  (error) => {
+    console.log('Ocurrió un error al cargar el OBJ:', error);  // Manejo de errores
+  }
+);
 
 function animate() {
   requestAnimationFrame(animate)
